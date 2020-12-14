b27cbfe0b0e4593b73634ab813f2f1e0be503e9b
Rafael Pinto <rafael12wq@gmail.com>
2020-12-14 15:22:45 -0300

    Adiciona a entidade Usuario

---
+++ src/main/java/com/project/demo/dominio/Usuario.java
@@ -0,0 +1,130 @@
+package com.project.demo.dominio;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nomeEmpresa;
+	private String cnpj;
+	private String telefone;
+	private String nomeResponsavel;
+	private String email;
+	
+	 
+	public Usuario() {
+		super();
+	}
+
+
+	public Usuario(Integer id, String nomeEmpresa, String cnpj, String telefone, String nomeResponsavel, String email) {
+		super();
+		this.id = id;
+		this.nomeEmpresa = nomeEmpresa;
+		this.cnpj = cnpj;
+		this.telefone = telefone;
+		this.nomeResponsavel = nomeResponsavel;
+		this.email = email;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+
+
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getNomeResponsavel() {
+		return nomeResponsavel;
+	}
+
+
+	public void setNomeResponsavel(String nomeResponsavel) {
+		this.nomeResponsavel = nomeResponsavel;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
